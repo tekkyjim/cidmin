71fd7cf2ba8efae3a95bc5463d4fcf98a4bfb00a
James Mitchell <tekkyjim@gmail.com>
2009-10-27 16:25:05 +0000

    added clearance views, making it formtastic!

--- config/environment.rb
+++ config/environment.rb
@@ -14,6 +14,7 @@ Rails::Initializer.run do |config|
   config.gem 'ambethia-smtp-tls', :lib => 'smtp-tls', :source => 'http://gems.github.com', :version => '>= 1.1.2'
   config.gem 'thoughtbot-paperclip', :lib => 'paperclip', :source => 'http://gems.github.com', :version => '>= 2.3.1'
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :version => '>= 1.2.2'
+  config.gem 'formtastic', :source => 'http://gemcutter.org'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
