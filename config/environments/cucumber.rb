a2c7a6a9c352356ae4f55b2f290774d56db92bff
James Mitchell <tekkyjim@gmail.com>
2009-10-27 15:26:38 +0000

    add rspec

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -24,3 +24,6 @@ config.gem 'webrat',      :lib => false,        :version => '>=0.5.0' unless Fil
 config.gem 'rspec',       :lib => false,        :version => '>=1.2.8' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem 'rspec-rails', :lib => false,        :version => '>=1.2.7.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 
+
+config.gem 'rspec', :lib => false, :version => '>= 1.2.9'
+config.gem 'rspec-rails', :lib => false, :version => '>= 1.2.9'
