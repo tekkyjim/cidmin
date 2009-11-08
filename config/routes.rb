754bebc1a8d17606688d6ece5a6ebe79416a4481
James Mitchell <tekkyjim@gmail.com>
2009-11-08 23:43:13 +0000

    added transactions to menu, little refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :transactions
-
   map.resources :cities
   map.resources :users
-  map.root :controller => 'high_voltage/pages', :action => 'show', :id => 'home'
   map.resources :characters
+  map.root :controller => 'high_voltage/pages', :action => 'show', :id => 'home'
 end
