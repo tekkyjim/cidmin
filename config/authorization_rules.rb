b046b421212a0bd7c09f8c587880dddf10c0aeab
James Mitchell <tekkyjim@gmail.com>
2009-11-21 16:46:53 +0000

    sorting roles, getting interface to make sense

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -6,22 +6,24 @@ authorization do
   end
   
   role :guest do
-      has_permission_on :users, :to => [:create, :new]
+        has_permission_on :users, :to => [:create, :new]
+  end
+
+  role :member do
       has_permission_on :users, :to => [:edit, :update, :show] do
         if_attribute :email => is { user.email }
       end
+      has_permission_on :characters, :to => [:create, :new]
+      has_permission_on :characters, :to => [:edit, :update, :show] do
+        if_attribute :user_id => is { user.id }
+      end
   end
 
-    role :moderator do
-      includes :guest
-      has_permission_on :comments, :to => [:edit, :update]
-    end
-
-    role :author do
-      includes :guest
-      has_permission_on :articles, :to => [:new, :create]
-      has_permission_on :articles, :to => [:edit, :update] do
-        if_attribute :user => is { user }
+  role :author do
+        includes :guest
+        has_permission_on :articles, :to => [:new, :create]
+        has_permission_on :articles, :to => [:edit, :update] do
+          if_attribute :user => is { user }
+        end
       end
-    end
 end
