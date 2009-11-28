200dd4bf762d9f1f656f75726b1941bf62d3a46a
James Mitchell <tekkyjim@gmail.com>
2009-11-28 21:19:22 +0000

    fixing more stuff

--- config/initializers/hoptoad.rb
+++
@@ -1,3 +0,0 @@
-HoptoadNotifier.configure do |config|
-   config.api_key = 'd7effc8079c63cfef9045b71fa3e0db5'
- end
--- features/step_definitions/characters_steps.rb
+++
@@ -1,5 +0,0 @@
-When /^I create a character named "([^\"]*)"$/ do |name|
-  fill_in :name, :with => name
-  click_button 'Create'
-end
-
