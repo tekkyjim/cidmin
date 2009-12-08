7b2123c9b139ca06a6b31150bebce6c29f2d8158
James Mitchell <tekkyjim@gmail.com>
2009-12-08 01:40:03 +0000

    seems to be working

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,29 @@
 class TransactionsController < ApplicationController
   filter_resource_access
-  def index
-    @transactions = Transaction.all
+  def formatted_created_at
+    created_at.strftime("%Y-%m-%d")
   end
+
+  def index
+    transactions = Transaction.find(:all) do
+      if params[:_search] == "true"
+        user_id        =~ "%#{params[:user_id]}%" if params[:user_id].present?
+        character_id   =~ "%#{params[:character_id]}%" if params[:character_id].present?
+        amount         =~ "%#{params[:amount]}%" if params[:amount].present?                
+        reason         =~ "%#{params[:reason]}%" if params[:reason].present?
+        created_at     =~ "%#{params[:created_at]}%" if params[:created_at].present?        
+      end
+      paginate :page => params[:page], :per_page => params[:rows]      
+      order_by "#{params[:sidx]} #{params[:sord]}"
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => transactions.to_jqgrid_json([:id,"user.name","character.name",:amount,:reason,"created_at.to_s"], 
+                                                         params[:page], params[:rows], transactions.total_entries) }
+    end
+  end
+  
   
   def show
     @transaction = Transaction.find(params[:id])
