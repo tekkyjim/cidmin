550f4759aef9d40b8813d23b9042591fc46c2761
James Mitchell <tekkyjim@gmail.com>
2009-12-15 14:44:31 +0000

    tidying before adding new features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-def next_game(city)
-  game = Games.find_by_city_id(city)
-  
-end
+  def next_game(city)
+    game = Game.find_by_city_id(city)
+  end
 end
